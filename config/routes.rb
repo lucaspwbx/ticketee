31b85bec61d999b03b14277d2f192119893652c1
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-23 18:35:18 -0300

    Implemented sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   root "projects#index"
+
+  resources :users
 
   resources :projects do
     resources :tickets
