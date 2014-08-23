a99fbcf9e74acbe06394238c6114fcbf6ba407c8
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-23 19:44:25 -0300

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   get 'users/show'
 
+  get '/signin', to: "sessions#new"
+  post '/signin', to: "sessions#create"
+
   root "projects#index"
 
   resources :users
