d4eda27f22a9ce3c458db77297723ebdc5a6cbba
Lucas Weiblen <lucasweiblen@gmail.com>
2014-08-23 17:29:08 -0300

    Implement displaying a list of relevant tickets

--- spec/factories/project_factory.rb
+++ spec/factories/project_factory.rb
@@ -2,4 +2,9 @@ FactoryGirl.define do
   factory :project do
     name "Example project"
   end
+
+  factory :ticket do
+    title "Example ticket"
+    description "An example ticket, nothing more"
+  end
 end
